c0e4dff64c00e0176e25b48103df82007da50a31
clement <clemohiomoje@gmail.com>
2019-04-26 15:26:56 +0100

    Episode component added and <p> from json file remove using the replace method

---
+++ src/Episodes.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import episodes from "./data/episodes.json";
+class Episodes extends Component {
+  render() {
+    return (
+      <div>
+        {episodes.map(episode => {
+          return (
+            <div className="episodes__container">
+              <h4>
+                <a
+                  href="true"
+                  name={`S${episode.season}E${episode.number}`}
+                >
+                  S{episode.season}E{episode.number}: {episode.name}
+                </a>
+              </h4>
+              <a href={episode.url}>
+                <img
+                  src={episode.image ? episode.image.medium : null}
+                  alt=""
+                />
+              </a>
+              <p className="summary__p">
+                {episode.summary
+                  ? episode.summary
+                      .replace(/<p>/g, "")
+                      .replace(/<\/p>/g, "")
+                  : null}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+export default Episodes;
